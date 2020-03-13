5132add51253acf13cc15744a32c75bcc26c98c3
Mindaugas Laganeckas <mindaugas.laganeckas@3shape.com>
2020-03-13 08:49:09 +0100

    Publish to nuget.org

--- build.ps1
+++ build.ps1
@@ -17,3 +17,4 @@ exec dotnet restore ./Source/GreetingTasks/
 exec dotnet pack -c Release ./Source/GreetingTasks/
 exec dotnet restore ./Example/
 exec dotnet msbuild /nologo '/t:Greet;Thank' ./Example/
+exec dotnet nuget push artifacts/*.nupkg -k $NUGET_ORG_KEY -s https://api.nuget.org/v3/index.json
